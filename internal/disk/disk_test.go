4e54b62ddf37f9b8e24c08aef75e5f382dad31fe
Andreas Signer <asigner@gmail.com>
2025-11-09 20:50:05 +0100

    cleanup

--- internal/disk/disk_test.go
+++ internal/disk/disk_test.go
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Oberon Disk Image Tool.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package disk
 
 import (
